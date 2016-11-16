36efe21390e1ffbfb089684d6cfa2e2cb249eb83
Joanna Sołomiewicz <joanna.solomiewicz@student.put.poznan.pl>
2016-11-16 11:30:29 +0100

    Video streaming from file/webcam

--- project.py
+++ project.py
@@ -1,5 +1,22 @@
+import cv2
+
+
 def main():
-    print("init")
+    # cap = cv2.VideoCapture('test2.mp4')
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
