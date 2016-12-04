d3b9377d88db9b8de3a7a2a76231a3286c6045f7
mpiniarski <piniarski.m@gmail.com>
2016-12-04 18:26:26 +0100

    Range detection and separation tests

--- feed.py
+++ feed.py
@@ -4,17 +4,14 @@ import os
 import sys
 from separator.separator import BlackBackgroundImageSeparator
 from detector.detector import FeatureDetector
-from detector.feature import Feature
 from repository.repository import FeatureRepository
 import sqlite3
-
 
 def main():
     args = get_args()
     directory_path = get_directory_path(args)
     db_path = get_db_path(args)
     image_file_names = get_jpg_from_directory(directory_path)
-
     connection = sqlite3.connect(db_path)
 
     separator = BlackBackgroundImageSeparator()
@@ -22,9 +19,6 @@ def main():
     feature_repository = FeatureRepository(connection)
 
     feature_repository.create_table_if_not_exists()
-    # feautre1 = Feature((10, 12, 13), [2.3, 1, 2, 3, 4, 1, 1])
-    # feature_repository.add(feautre1, 'banana')
-    # features = feature_repository.find_all()
 
     for file_name in image_file_names:
         image = cv2.imread(directory_path + "/" + file_name)
@@ -32,15 +26,21 @@ def main():
             continue
 
         contour = separator.separate(image)
-
-        cv2.imshow(file_name, detector.get_mask(contour, image))
-
+        # delete later
+        # cv2.imshow(file_name, detector._get_mask(contour, image))
         feature = detector.calculate_features(image, contour)
+        color_ranges = detector.get_color_ranges_in_contour(contour, image)
         fruit_name = file_name.split('.')[0]
         feature_repository.add(feature, fruit_name)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+    # features = feature_repository.find_all()
+    # for f in features:
+    #     print(f.mean_color)
+    #     print(f.hu_moments)
+
     connection.close()
 
 
--- separator/separator.py
+++ separator/separator.py
@@ -55,14 +55,16 @@ class ColorBasedImageSeparator:
 
 class ImageSeparator:
     def __init__(self):
-        self.threshold1 = 30
-        self.threshold2 = 40
+        self.threshold1 = 50
+        self.threshold2 = 150
 
         self.contour_to_image_size_ratio = 0.01
 
     def canny_separate_objects(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blur = cv2.blur(gray, (3, 3))
+        blur = cv2.GaussianBlur(image, (3, 3), 0)
+        blur = cv2.GaussianBlur(blur, (3, 3), 0)
+        cv2.imshow('blur', blur)
         # blur = cv2.bilateralFilter(gray, 11, 17, 17)
         # blur = cv2.medianBlur(grey, 3)
         canny = cv2.Canny(blur, self.threshold1, self.threshold2)
@@ -72,48 +74,59 @@ class ImageSeparator:
 
         _, contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        image_width, image_height = image.shape[:2]
-        image_area = image_width * image_height
-        contour_to_image_size_ratio = 0.1
+        return contours
 
-        outer_contours = []
-        for i, contour in enumerate(contours):
-            if hierarchy[0][i][3] == -1:  # only contours without parents (outer ones)
-                if cv2.contourArea(contour) / image_area > contour_to_image_size_ratio:
-                    outer_contours.append(contour)
-        return outer_contours
+        # image_width, image_height = image.shape[:2]
+        # image_area = image_width * image_height
+        # contour_to_image_size_ratio = 0.01
+        #
+        # outer_contours = []
+        # for i, contour in enumerate(contours):
+        #     if hierarchy[0][i][3] == -1:  # only contours without parents (outer ones)
+        #         if cv2.contourArea(contour) / image_area > contour_to_image_size_ratio:
+        #             outer_contours.append(contour)
+        # return outer_contours
 
     def saturation_separate_objects(self, image):
         # image = cv2.blur(image, (3, 3))
         # image = cv2.bilateralFilter(image, 11, 17, 17)
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        hsv = cv2.GaussianBlur(hsv, (3, 3), 0)
+        blur = cv2.GaussianBlur(image, (3, 3), 0)
+        cv2.imshow('blur', blur)
+        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
         # saturation = [element[1] for row in hsv for element in row]
         # grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         hue = self.extract_channel(hsv, 0)
         saturation = self.extract_channel(hsv, 1)
+        cv2.imshow('saturation', saturation)
         value = self.extract_channel(hsv, 2)
         # cv2.imshow('hue', hue)
         # cv2.imshow('saturation', saturation)
         # cv2.imshow('value', value)
         # _, thr = cv2.threshold(saturation, 80, 255, cv2.THRESH_BINARY)
         # thr = cv2.adaptiveThreshold(saturation, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        blur = cv2.GaussianBlur(saturation, (5, 5), 0)
-        _, thr = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        _, thr = cv2.threshold(saturation, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        cv2.imshow('thr', thr)
+        kernel = np.ones((3, 3), np.uint8)
+        thr = cv2.morphologyEx(thr, cv2.MORPH_OPEN, kernel, iterations=2)
+        cv2.imshow('thr-close', thr)
+
         # canny = cv2.Canny(value, 80, 200)
         # cv2.imshow('hsv', hsv)
         # cv2.imshow('thr', thr)
 
-        image_width, image_height = image.shape[:2]
-        image_area = image_width * image_height
-
         _, contours, hierarchy = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        outer_contours = []
-        for i, contour in enumerate(contours):
-            if hierarchy[0][i][3] == -1:  # only contours without parents (outer ones)
-                if cv2.contourArea(contour) / image_area > 0.2:
-                    outer_contours.append(contour)
-        return outer_contours
+        return contours
+
+        # image_width, image_height = image.shape[:2]
+        # image_area = image_width * image_height
+        #
+        # outer_contours = []
+        # for i, contour in enumerate(contours):
+        #     if hierarchy[0][i][3] == -1:  # only contours without parents (outer ones)
+        #         if cv2.contourArea(contour) / image_area > 0.2:
+        #             outer_contours.append(contour)
+        # return outer_contours
 
     def extract_channel(self, hsv, channel):
         return np.asarray([[element[channel] for element in row] for row in hsv])
@@ -123,7 +136,6 @@ class BlackBackgroundImageSeparator:
     def separate(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, thr = cv2.threshold(gray, 10, 255, cv2.THRESH_BINARY)
-        cv2.imshow("aa", thr)
         _, contours, hierarchy = cv2.findContours(thr, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         image_width, image_height = image.shape[:2]
